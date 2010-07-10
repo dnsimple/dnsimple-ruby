d0fbf716d89174f4971f1c4edaec26c12ae31124
Anthony Eden <anthonyeden@gmail.com>
2010-07-10 10:49:53 -1000

    add support for :template option

--- lib/dnsimple/commands/create_domain.rb
+++ lib/dnsimple/commands/create_domain.rb
@@ -5,6 +5,11 @@ module DNSimple
         name = args.shift
         domain = Domain.create(name) 
         puts "Created #{domain.name}"
+
+        if template = options.delete(:template)
+          domain.apply(template)
+          puts "Applied template #{template} to #{domain.name}"
+        end
       end
     end
   end
