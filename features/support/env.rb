72ec8774f9ad0859fdd950d1d1afc242bdb425b5
Anthony Eden <anthonyeden@gmail.com>
2011-03-17 23:49:55 +0100

    NODELETE env option to keep the records created during testing

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,10 +1,14 @@
 require 'aruba/cucumber'
+
 Before do
   @aruba_timeout_seconds = 10
 end
+
 After do
-  if @domain_name && (@domain_name =~ /^cli-/ || @domain_name =~ /in-addr\.arpa/)
-    steps %Q(When I run `dnsimple delete #{@domain_name}`)
+  unless ENV['NODELETE']
+    if @domain_name && (@domain_name =~ /^cli-/ || @domain_name =~ /in-addr\.arpa/)
+      steps %Q(When I run `dnsimple delete #{@domain_name}`)
+    end
   end
 end
 
