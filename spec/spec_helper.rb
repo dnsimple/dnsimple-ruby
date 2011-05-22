97703287f46da34bd4253a7147316d3444c0f6c2
Anthony Eden <anthonyeden@gmail.com>
2011-05-22 11:31:07 -0400

    use .dnsimple.test and modify the defaul base_uri when running specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require 'lib/dnsimple'
 
-config = YAML.load(File.new(File.expand_path('~/.dnsimple.localhost')))
+config = YAML.load(File.new(File.expand_path('~/.dnsimple.test')))
 
-DNSimple::Client.base_uri = config['site'] || "http://localhost:3000/" 
+DNSimple::Client.base_uri = config['site'] || "https://test.dnsimple.com/" 
 DNSimple::Client.username = config['username']
 DNSimple::Client.password = config['password'] 
