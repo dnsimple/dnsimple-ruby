eaec838ff67e1dc4bfb57449302d6beb8ead38a1
Anthony Eden <anthonyeden@gmail.com>
2011-05-22 17:02:49 -0400

    use vcr for template

--- spec/template_spec.rb
+++ spec/template_spec.rb
@@ -2,6 +2,7 @@ require File.join(File.dirname(__FILE__), 'spec_helper')
 
 describe DNSimple::Template do
   describe "a template" do
+    use_vcr_cassette
     it "can be found by name" do
       template = DNSimple::Template.find('googleapps')
       template.id.should_not be_nil
