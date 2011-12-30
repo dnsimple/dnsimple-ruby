05bb1dcd4e405190830049fc95d09aae4679b042
Pat Allan <pat@freelancing-gods.com>
2011-12-30 14:16:21 +1100

    Fixing broken spec.
    Don't think this was my fault. Still, it's green now.

--- spec/commands/purchase_certificate_spec.rb
+++ spec/commands/purchase_certificate_spec.rb
@@ -3,12 +3,24 @@ require 'dnsimple/certificate'
 require 'dnsimple/commands/purchase_certificate'
 
 describe DNSimple::Commands::PurchaseCertificate do
-  let(:out) { StringIO.new }
+  let(:out)         { StringIO.new }
   let(:domain_name) { 'example.com' }
+  let(:domain)      { stub('domain') }
+  let(:contact)     { stub('contact') }
+
   context "with one argument" do
-    xit "purchases the certificate" do
-      DNSimple::Certificate.expects(:purchase).with(domain_name, '').returns(stub("certificate", :fqdn => domain_name))
-      DNSimple::Commands::PurchaseCertificate.new(out).execute([domain_name])
+    before :each do
+      DNSimple::Domain.stubs(:find).returns(domain)
+      DNSimple::Contact.stubs(:find).returns(contact)
+    end
+
+    it "purchases the certificate" do
+      DNSimple::Certificate.expects(:purchase).
+        with(domain, 'certname', contact).
+        returns(stub("certificate", :fqdn => domain_name))
+
+      DNSimple::Commands::PurchaseCertificate.new(out).
+        execute([domain_name, 'certname', stub('contact id')])
     end
   end
 end
