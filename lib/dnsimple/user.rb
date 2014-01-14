2b4e9293a358347cce93c109b67850a3b1131c9e
Simone Carletti <weppos@weppos.net>
2014-01-14 19:00:55 +0100

    Replace User VCR cassettes with fixtures

--- lib/dnsimple/user.rb
+++ lib/dnsimple/user.rb
@@ -2,29 +2,21 @@ module DNSimple
   class User < Base
 
     attr_accessor :id
-
+    attr_accessor :email
+    attr_accessor :domain_count
+    attr_accessor :domain_limit
+    attr_accessor :login_count
+    attr_accessor :failed_login_count
     attr_accessor :created_at
-
     attr_accessor :updated_at
 
-    attr_accessor :email
 
-    attr_accessor :login_count
-
-    attr_accessor :failed_login_count
-
-    attr_accessor :domain_count
-
-    attr_accessor :domain_limit
-
-    def self.me(options={})
-      response = DNSimple::Client.get("users/me", options)
+    def self.me
+      response = DNSimple::Client.get("users/me")
 
       case response.code
       when 200
         new(response["user"])
-      when 404
-        raise RecordNotFound, "Could not find account with current credentials"
       else
         raise RequestError.new("Error finding account", response)
       end
