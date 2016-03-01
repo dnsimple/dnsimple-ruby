1c099962413ecec48d75923768e5f762ac103b5d
Javier Acero <j4cegu@gmail.com>
2016-03-01 19:25:06 +0100

    Make #check the first method of registrar module

--- lib/dnsimple/client/registrar.rb
+++ lib/dnsimple/client/registrar.rb
@@ -2,25 +2,6 @@ module Dnsimple
   class Client
     module Registrar
 
-      # Registers a domain.
-      #
-      # @see https://developer.dnsimple.com/v2/registrar/#register
-      #
-      # @param  [Fixnum] account_id the account ID
-      # @param  [#to_s] domain_name The domain name to register.
-      # @param  [Hash] attributes
-      # @param  [Hash] options
-      # @return [Struct::Domain]
-      #
-      # @raise  [RequestError] When the request fails.
-      def register(account_id, domain_name, attributes = {}, options = {})
-        Extra.validate_mandatory_attributes(attributes, [:registrant_id])
-        options  = options.merge(attributes)
-        response = client.post(Client.versioned("/%s/registrar/domains/%s/registration" % [account_id, domain_name]), options)
-
-        Dnsimple::Response.new(response, Struct::Domain.new(response["data"]))
-      end
-
       # Checks whether a domain is available to be registered.
       #
       # @see https://developer.dnsimple.com/v2/registrar/#check
@@ -38,6 +19,25 @@ module Dnsimple
         response = client.get(Client.versioned("/%s/registrar/domains/%s/check" % [account_id, domain_name]), options)
 
         Dnsimple::Response.new(response, Struct::DomainCheck.new(response["data"]))
+      end
+
+      # Registers a domain.
+      #
+      # @see https://developer.dnsimple.com/v2/registrar/#register
+      #
+      # @param  [Fixnum] account_id the account ID
+      # @param  [#to_s] domain_name The domain name to register.
+      # @param  [Hash] attributes
+      # @param  [Hash] options
+      # @return [Struct::Domain]
+      #
+      # @raise  [RequestError] When the request fails.
+      def register(account_id, domain_name, attributes = {}, options = {})
+        Extra.validate_mandatory_attributes(attributes, [:registrant_id])
+        options  = options.merge(attributes)
+        response = client.post(Client.versioned("/%s/registrar/domains/%s/registration" % [account_id, domain_name]), options)
+
+        Dnsimple::Response.new(response, Struct::Domain.new(response["data"]))
       end
 
     end
--- spec/dnsimple/client/registrar_spec.rb
+++ spec/dnsimple/client/registrar_spec.rb
@@ -5,6 +5,33 @@ describe Dnsimple::Client, ".registrar" do
   subject { described_class.new(base_url: "https://api.dnsimple.test", access_token: "a1b2c3").registrar }
 
 
+  describe "#check" do
+    let(:account_id) { 1010 }
+
+    before do
+      stub_request(:get, %r[/v2/#{account_id}/registrar/domains/.+/check$])
+          .to_return(read_http_fixture("checkDomain/success.http"))
+    end
+
+    it "builds the correct request" do
+      subject.check(account_id, domain_name = "example.com")
+
+      expect(WebMock).to have_requested(:get, "https://api.dnsimple.test/v2/#{account_id}/registrar/domains/#{domain_name}/check")
+          .with(headers: { 'Accept' => 'application/json' })
+    end
+
+    it "returns the availability" do
+      response = subject.check(account_id, "example.com")
+      expect(response).to be_a(Dnsimple::Response)
+
+      result = response.data
+      expect(result).to be_a(Dnsimple::Struct::DomainCheck)
+      expect(result.domain).to eq("example.com")
+      expect(result.available).to be_truthy
+      expect(result.premium).to be_falsey
+    end
+  end
+
   describe "#register" do
     let(:account_id) { 1010 }
 
@@ -30,33 +57,6 @@ describe Dnsimple::Client, ".registrar" do
       result = response.data
       expect(result).to be_a(Dnsimple::Struct::Domain)
       expect(result.id).to be_a(Fixnum)
-    end
-  end
-
-  describe "#check" do
-    let(:account_id) { 1010 }
-
-    before do
-      stub_request(:get, %r[/v2/#{account_id}/registrar/domains/.+/check$])
-          .to_return(read_http_fixture("checkDomain/success.http"))
-    end
-
-    it "builds the correct request" do
-      subject.check(account_id, domain_name = "example.com")
-
-      expect(WebMock).to have_requested(:get, "https://api.dnsimple.test/v2/#{account_id}/registrar/domains/#{domain_name}/check")
-          .with(headers: { 'Accept' => 'application/json' })
-    end
-
-    it "returns the availability" do
-      response = subject.check(account_id, "example.com")
-      expect(response).to be_a(Dnsimple::Response)
-
-      result = response.data
-      expect(result).to be_a(Dnsimple::Struct::DomainCheck)
-      expect(result.domain).to eq("example.com")
-      expect(result.available).to be_truthy
-      expect(result.premium).to be_falsey
     end
   end
 
