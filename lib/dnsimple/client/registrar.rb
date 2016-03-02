cdc3e15b82a81026b31ac5748e9cd7ad69f97632
Javier Acero <j4cegu@gmail.com>
2016-03-02 08:29:09 +0100

    Add registrar/transferDomain documentation

--- lib/dnsimple/client/registrar.rb
+++ lib/dnsimple/client/registrar.rb
@@ -6,7 +6,7 @@ module Dnsimple
       #
       # @see https://developer.dnsimple.com/v2/registrar/#check
       #
-      # @example Check whether example.com is available.
+      # @example Check whether example.com is available:
       #   client.registrar.check(1010, "example.com")
       #
       # @param  [Fixnum] account_id the account ID
@@ -40,6 +40,20 @@ module Dnsimple
         Dnsimple::Response.new(response, Struct::Domain.new(response["data"]))
       end
 
+      # Starts the transfer of a domain to DNSimple.
+      #
+      # @see https://developer.dnsimple.com/v2/registrar/#transfer
+      #
+      # @example Initiate the transfer for example.com using the contact 1234 as registrant:
+      #   client.registrar.transfer(1010, "example.com", registrant_id: 1234, auth_info: "x1y2z3")
+      #
+      # @param  [Fixnum] account_id the account ID
+      # @param  [#to_s] domain_name The domain name to transfer.
+      # @param  [Hash] attributes
+      # @param  [Hash] options
+      # @return [Struct::Domain]
+      #
+      # @raise  [RequestError] When the request fails.
       def transfer(account_id, domain_name, attributes = {}, options = {})
         Extra.validate_mandatory_attributes(attributes, [:registrant_id, :auth_info])
         options  = options.merge(attributes)
