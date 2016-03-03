a23156888b7df300aa73e26a424497123cb2b56e
Javier Acero <j4cegu@gmail.com>
2016-03-03 12:22:08 +0100

    Implement getWhoisPrivacy

--- lib/dnsimple/client/registrar_whois_privacy.rb
+++ lib/dnsimple/client/registrar_whois_privacy.rb
@@ -1,6 +1,14 @@
 module Dnsimple
   class Client
     module RegistrarWhoisPrivacy
+
+      def get_whois_privacy(account_id, domain_name, options = {})
+        endpoint = Client.versioned("/%s/registrar/domains/%s/whois_privacy" % [account_id, domain_name])
+        response = client.get(endpoint, options)
+
+        Dnsimple::Response.new(response, Struct::WhoisPrivacy.new(response["data"]))
+      end
+
     end
   end
 end
