29f745309bb72400aebadc10bdb04f6407e26058
Javier Acero <j4cegu@gmail.com>
2016-03-03 12:26:33 +0100

    Add documentation to Registrar#get_whois_privacy

--- lib/dnsimple/client/registrar_whois_privacy.rb
+++ lib/dnsimple/client/registrar_whois_privacy.rb
@@ -2,6 +2,19 @@ module Dnsimple
   class Client
     module RegistrarWhoisPrivacy
 
+      # Gets the whois privacy for the domain
+      #
+      # @see https://developer.dnsimple.com/v2/registrar/whois-privacy/#get
+      #
+      # @example Get the whois privacy for "example.com":
+      #   client.registrar.get_whois_privacy(1010, "example.com")
+      #
+      # @param  [Fixnum] account_id the account ID
+      # @param  [#to_s] domain_name The domain name to check.
+      # @param  [Hash] options
+      # @return [Struct::WhoisPrivacy]
+      #
+      # @raise  [RequestError] When the request fails.
       def get_whois_privacy(account_id, domain_name, options = {})
         endpoint = Client.versioned("/%s/registrar/domains/%s/whois_privacy" % [account_id, domain_name])
         response = client.get(endpoint, options)
