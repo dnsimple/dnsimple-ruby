ff16dce1969cbc7c56c350fde7fbcd57edc98125
Simone Carletti <weppos@weppos.net>
2016-03-27 01:27:23 +0100

    Rubocop: Fix Style/ClosingParenthesisIndentation

--- spec/dnsimple/client_spec.rb
+++ spec/dnsimple/client_spec.rb
@@ -127,39 +127,42 @@ describe Dnsimple::Client do
       stub_request(:get, %r[/foo])
 
       expect(HTTParty).to receive(:get).
-                          with("#{subject.base_url}foo",
-                               format: :json,
-                               basic_auth: { username: "user", password: "pass" },
-                               headers: { 'Accept' => 'application/json', 'User-Agent' => "dnsimple-ruby/#{Dnsimple::VERSION}" }
-                          ).
-                          and_return(double('response', code: 200))
+          with(
+            "#{subject.base_url}foo",
+            format: :json,
+            basic_auth: { username: "user", password: "pass" },
+            headers: { 'Accept' => 'application/json', 'User-Agent' => "dnsimple-ruby/#{Dnsimple::VERSION}" }
+          ).
+          and_return(double('response', code: 200))
 
       subject.request(:get, 'foo')
     end
 
     it "properly extracts processes options and encodes data" do
       expect(HTTParty).to receive(:put).
-                          with("#{subject.base_url}foo",
-                               format: :json,
-                               body: JSON.dump({ something: "else" }),
-                               query: { foo: "bar" },
-                               basic_auth: { username: "user", password: "pass" },
-                               headers: { 'Accept' => 'application/json', 'Content-Type' => 'application/json', 'User-Agent' => "dnsimple-ruby/#{Dnsimple::VERSION}", "Custom" => "Header" }
-                          ).
-                          and_return(double('response', code: 200))
+          with(
+            "#{subject.base_url}foo",
+            format: :json,
+            body: JSON.dump({ something: "else" }),
+            query: { foo: "bar" },
+            basic_auth: { username: "user", password: "pass" },
+            headers: { 'Accept' => 'application/json', 'Content-Type' => 'application/json', 'User-Agent' => "dnsimple-ruby/#{Dnsimple::VERSION}", "Custom" => "Header" }
+          ).
+          and_return(double('response', code: 200))
 
       subject.request(:put, 'foo', { something: "else" }, { query: { foo: "bar" }, headers: { "Custom" => "Header" } })
     end
 
     it "handles non application/json content types" do
       expect(HTTParty).to receive(:post).
-                          with("#{subject.base_url}foo",
-                               format: :json,
-                               body: { something: "else" },
-                               basic_auth: { username: "user", password: "pass" },
-                               headers: { 'Accept' => 'application/json', 'Content-Type' => 'application/x-www-form-urlencoded', 'User-Agent' => "dnsimple-ruby/#{Dnsimple::VERSION}" }
-                          ).
-                          and_return(double('response', code: 200))
+          with(
+            "#{subject.base_url}foo",
+            format: :json,
+            body: { something: "else" },
+            basic_auth: { username: "user", password: "pass" },
+            headers: { 'Accept' => 'application/json', 'Content-Type' => 'application/x-www-form-urlencoded', 'User-Agent' => "dnsimple-ruby/#{Dnsimple::VERSION}" }
+          ).
+          and_return(double('response', code: 200))
 
       subject.request(:post, 'foo', { something: "else" }, headers: { "Content-Type" => "application/x-www-form-urlencoded" })
     end
