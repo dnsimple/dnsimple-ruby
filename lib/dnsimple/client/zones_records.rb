3a3bde282bba4abc89ab8bce6e68e5e93936a99f
Javier Acero <j4cegu@gmail.com>
2016-05-04 13:32:34 +0200

    Add missing aliases in zone records endpoints
    To keep them consistent with the rest of the library

--- lib/dnsimple/client/zones_records.rb
+++ lib/dnsimple/client/zones_records.rb
@@ -24,6 +24,7 @@ module Dnsimple
 
         Dnsimple::PaginatedResponse.new(response, response["data"].map { |r| Struct::Record.new(r) })
       end
+      alias list records
       alias list_records records
 
       # Lists ALL the zone records in the account.
@@ -46,6 +47,7 @@ module Dnsimple
       def all_records(account_id, zone_id, options = {})
         paginate(:records, account_id, zone_id, options)
       end
+      alias all all_records
 
       # Creates a zone record in the account.
       #
@@ -64,6 +66,7 @@ module Dnsimple
 
         Dnsimple::Response.new(response, Struct::Record.new(response["data"]))
       end
+      alias create create_record
 
       # Gets a zone record from the account.
       #
@@ -101,6 +104,7 @@ module Dnsimple
 
         Dnsimple::Response.new(response, Struct::Record.new(response["data"]))
       end
+      alias update update_record
 
       # Deletes a zone record from the account.
       #
@@ -121,6 +125,7 @@ module Dnsimple
 
         Dnsimple::Response.new(response, nil)
       end
+      alias delete delete_record
 
     end
   end
