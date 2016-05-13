bb0a152b463799ab47773453fb04a4c9be36307e
Javier Acero <j4cegu@gmail.com>
2016-05-13 13:02:01 +0200

    Remove unused configuration block from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,15 +12,4 @@ unless defined?(SPEC_ROOT)
   SPEC_ROOT = File.expand_path("../", __FILE__)
 end
 
-RSpec.configure do |c|
-  # Silent the puts call in the commands
-  # c.before do
-  #   @_stdout = $stdout
-  #   $stdout = StringIO.new
-  # end
-  # c.after do
-  #   $stdout = @_stdout
-  # end
-end
-
 Dir[File.join(SPEC_ROOT, "support/**/*.rb")].each { |f| require f }
