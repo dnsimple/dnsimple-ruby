5347a6498d50a908786d3bf11c22a6258015e856
Javier Acero <j4cegu@gmail.com>
2016-06-15 10:28:55 +0200

    Adds support for the accounts endpoint (#113)
    * Copy listAccounts fixtures from developer repo
    
    * Add support for the accounts endpoint
    
    * Add plan identifier to the account
    
    * Add docs to the endpoint
    
    * Fixed typo in spec description

--- lib/dnsimple/struct/account.rb
+++ lib/dnsimple/struct/account.rb
@@ -7,6 +7,9 @@ module Dnsimple
 
       # @return [String] The account email.
       attr_accessor :email
+
+      # @return [String] The identifier of the plan the account is subscribed to.
+      attr_accessor :plan_identifier
     end
 
   end
