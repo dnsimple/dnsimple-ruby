b8e796b813dcc0ea0cab402afa0e6a2d42519d40
Luca Guidi <me@lucaguidi.com>
2016-07-21 10:51:03 +0200

    Added support for `applyTemplate`

--- lib/dnsimple/client/templates.rb
+++ lib/dnsimple/client/templates.rb
@@ -146,6 +146,28 @@ module Dnsimple
       end
       alias delete delete_template
 
+      # Applies a template to the domain.
+      #
+      # @see https://developer.dnsimple.com/v2/templates/domains/#apply
+      #
+      # @example Apply template 5401 for example.com in account 1010:
+      #   client.templates.apply_template(1010, "example.com", 5401)
+      #
+      # @param  [Fixnum] account_id The account ID
+      # @param  [#to_s] domain_id The Domain ID or name
+      # @param  [#to_s] template_id The template ID
+      # @param  [Hash] options
+      # @return [Dnsimple::Response<nil>]
+      #
+      # @raise  [Dnsimple::NotFoundError]
+      # @raise  [Dnsimple::RequestError]
+      def apply_template(account_id, domain_id, template_id, options = {})
+        endpoint = Client.versioned("/%s/domains/%s/templates/%s" % [account_id, domain_id, template_id])
+        response = client.post(endpoint, options)
+
+        Dnsimple::Response.new(response, nil)
+      end
+
     end
   end
 end
