b33e06b1c371af563eb799ac416c87f64fde53be
Javier Acero <j4cegu@gmail.com>
2016-07-22 16:23:40 +0200

    Swap template_id and domain_id arguments

--- spec/dnsimple/client/templates_spec.rb
+++ spec/dnsimple/client/templates_spec.rb
@@ -191,8 +191,8 @@ describe Dnsimple::Client, ".templates" do
 
   describe "#apply_template" do
     let(:account_id)  { 1010 }
-    let(:domain_id)   { 'example.com' }
     let(:template_id) { 5410 }
+    let(:domain_id)   { 'example.com' }
 
     before do
       stub_request(:post, %r{/v2/#{account_id}/domains/#{domain_id}/templates/#{template_id}$}).
@@ -200,14 +200,14 @@ describe Dnsimple::Client, ".templates" do
     end
 
     it "builds the correct request" do
-      subject.apply_template(account_id, domain_id, template_id)
+      subject.apply_template(account_id, template_id, domain_id)
 
       expect(WebMock).to have_requested(:post, "https://api.dnsimple.test/v2/#{account_id}/domains/#{domain_id}/templates/#{template_id}").
           with(headers: { "Accept" => "application/json" })
     end
 
     it "returns nil" do
-      response = subject.apply_template(account_id, domain_id, template_id)
+      response = subject.apply_template(account_id, template_id, domain_id)
       expect(response).to be_a(Dnsimple::Response)
       expect(response.data).to be_nil
     end
