8ccf7988ada322533287f59a8ebb075529f69b3c
Luca Guidi <me@lucaguidi.com>
2016-12-12 01:00:53 +0000

    Added support for getDomainPremiumPrice

---
+++ lib/dnsimple/struct/domain_premium_price.rb
@@ -0,0 +1,13 @@
+module Dnsimple
+  module Struct
+
+    class DomainPremiumPrice < Base
+      # @return [String] The domain premium price
+      attr_accessor :premium_price
+
+      # @return [String] The action: registration/transfer/renewal
+      attr_accessor :action
+    end
+
+  end
+end
