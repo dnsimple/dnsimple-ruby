7dfabbe455ca1e02e163438baf96f253c13d8b8b
Luca Guidi <me@lucaguidi.com>
2018-05-07 17:12:09 +0200

    Implement checkZoneRecordDistribution

--- lib/dnsimple/client/zones_distributions.rb
+++ lib/dnsimple/client/zones_distributions.rb
@@ -19,6 +19,27 @@ module Dnsimple
         Dnsimple::Response.new(response, Struct::ZoneDistribution.new(response["data"]))
       end
 
+      # Gets a zone record distribution state
+      #
+      # @see https://developer.dnsimple.com/v2/zones/records/#checkZoneRecordDistribution
+      #
+      # @example Get record 123 in zone "example.com"
+      #   client.zones.zone_record_distribution(1010, "example.com", 123)
+      #
+      # @param  [Integer] account_id the account ID
+      # @param  [String] zone_id the zone name
+      # @param  [Integer] record_id the record ID
+      # @param  [Hash] options
+      # @return [Dnsimple::Response<Dnsimple::Struct::ZoneRecordDistribution>]
+      #
+      # @raise  [Dnsimple::NotFoundError]
+      # @raise  [Dnsimple::RequestError]
+      def zone_record_distribution(account_id, zone_id, record_id, options = {})
+        response = client.get(Client.versioned("/%s/zones/%s/records/%s/distribution" % [account_id, zone_id, record_id]), options)
+
+        Dnsimple::Response.new(response, Struct::ZoneRecordDistribution.new(response["data"]))
+      end
+
     end
   end
 end
--- spec/dnsimple/client/zones_distributions_spec.rb
+++ spec/dnsimple/client/zones_distributions_spec.rb
@@ -61,4 +61,75 @@ describe Dnsimple::Client, ".zones" do
     end
   end
 
+
+  describe "#zone_record_distribution" do
+    let(:account_id) { 1010 }
+    let(:zone_id) { "example.com" }
+    let(:record_id) { 5 }
+
+    before do
+      stub_request(:get, %r{/v2/#{account_id}/zones/#{zone_id}/records/#{record_id}/distribution$})
+          .to_return(read_http_fixture("checkZoneRecordDistribution/success.http"))
+    end
+
+    it "builds the correct request" do
+      subject.zone_record_distribution(account_id, zone_id, record_id)
+
+      expect(WebMock).to have_requested(:get, "https://api.dnsimple.test/v2/#{account_id}/zones/#{zone_id}/records/#{record_id}/distribution")
+          .with(headers: { 'Accept' => 'application/json' })
+    end
+
+    it "returns the zone record distribution check with true when the zone is fully distributed" do
+      response = subject.zone_record_distribution(account_id, zone_id, record_id)
+      expect(response).to be_a(Dnsimple::Response)
+
+      result = response.data
+      expect(result).to be_a(Dnsimple::Struct::ZoneRecordDistribution)
+      expect(result.distributed).to be(true)
+    end
+
+    it "returns the zone distribution check with false when the zone isn't fully distributed" do
+      stub_request(:get, %r{/v2/#{account_id}/zones/#{zone_id}/records/#{record_id}/distribution$})
+          .to_return(read_http_fixture("checkZoneRecordDistribution/failure.http"))
+
+      response = subject.zone_record_distribution(account_id, zone_id, record_id)
+      expect(response).to be_a(Dnsimple::Response)
+
+      result = response.data
+      expect(result).to be_a(Dnsimple::Struct::ZoneRecordDistribution)
+      expect(result.distributed).to be(false)
+    end
+
+    it "raises an error when the server wasn't able to complete the check" do
+      stub_request(:get, %r{/v2/#{account_id}/zones/#{zone_id}/records/#{record_id}/distribution$})
+          .to_return(read_http_fixture("checkZoneRecordDistribution/error.http"))
+
+      expect {
+        subject.zone_record_distribution(account_id, zone_id, record_id)
+      }.to raise_error(Dnsimple::RequestError, "Could not query zone, connection timed out")
+    end
+
+    context "when the zone does not exist" do
+      it "raises NotFoundError" do
+        stub_request(:get, %r{/v2})
+            .to_return(read_http_fixture("notfound-zone.http"))
+
+        expect {
+          subject.zone_record_distribution(account_id, zone_id, "0")
+        }.to raise_error(Dnsimple::NotFoundError)
+      end
+    end
+
+    context "when the record does not exist" do
+      it "raises NotFoundError" do
+        stub_request(:get, %r{/v2})
+            .to_return(read_http_fixture("notfound-record.http"))
+
+        expect {
+          subject.zone_record_distribution(account_id, zone_id, "0")
+        }.to raise_error(Dnsimple::NotFoundError)
+      end
+    end
+  end
+
 end
