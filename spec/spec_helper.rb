e71f7ca501808bb1da3f121915c32d93310d3197
Santiago Traversa <santiago.traversa@gmail.com>
2021-04-22 19:38:38 -0300

    [Corrected] Lint/RedundantDirGlobSort
    Remove redundant sort.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,4 +14,4 @@ unless defined?(SPEC_ROOT)
   SPEC_ROOT = File.expand_path(__dir__)
 end
 
-Dir[File.join(SPEC_ROOT, "support/**/*.rb")].sort.each { |f| require f }
+Dir[File.join(SPEC_ROOT, "support/**/*.rb")].each { |f| require f }
