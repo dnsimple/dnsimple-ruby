6f56a2a153def447aa8008425b20b0ac15a2247c
Stella Lok <stellalok@gmail.com>
2025-09-10 18:20:12 +0800

    Add new structs

---
+++ lib/dnsimple/struct/zone_bulk_edit.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Dnsimple
+  module Struct
+    class ZoneBulkEdit < Base
+
+      # @return [Array<ZoneRecord>]
+      attr_accessor :creates
+
+      # @return [Array<ZoneRecord>]
+      attr_accessor :updates
+
+      # @return [Array<ZoneRecordId>]
+      attr_accessor :deletes
+
+    end
+  end
+end
