43df97b1dd4e697dbf66210ca0e31b0022b7092e
Simone Carletti <5387+weppos@users.noreply.github.com>
2026-01-14 12:09:01 +0100

    Removed deprecated getWhoisPrivacy and renewWhoisPrivacy (#447)
    See https://github.com/dnsimple/dnsimple-developer/pull/919

--- lib/dnsimple/client/registrar_whois_privacy.rb
+++ lib/dnsimple/client/registrar_whois_privacy.rb
@@ -3,26 +3,6 @@
 module Dnsimple
   class Client
     module RegistrarWhoisPrivacy
-      # Gets the whois privacy for the domain.
-      #
-      # @see https://developer.dnsimple.com/v2/registrar/whois-privacy/#get
-      #
-      # @example Get the whois privacy for "example.com":
-      #   client.registrar.whois_privacy(1010, "example.com")
-      #
-      # @param  [Integer] account_id the account ID
-      # @param  [#to_s] domain_name the domain name
-      # @param  [Hash] options
-      # @return [Struct::WhoisPrivacy]
-      #
-      # @raise  [RequestError] When the request fails.
-      def whois_privacy(account_id, domain_name, options = {})
-        endpoint = whois_privacy_endpoint(account_id, domain_name)
-        response = client.get(endpoint, options)
-
-        Dnsimple::Response.new(response, Struct::WhoisPrivacy.new(response["data"]))
-      end
-
       # Enables whois privacy for the domain.
       #
       # @see https://developer.dnsimple.com/v2/registrar/whois-privacy/#enable
@@ -61,26 +41,6 @@ module Dnsimple
         response = client.delete(endpoint, nil, options)
 
         Dnsimple::Response.new(response, Struct::WhoisPrivacy.new(response["data"]))
-      end
-
-      # Renews whois privacy for the domain.
-      #
-      # @see https://developer.dnsimple.com/v2/registrar/whois-privacy/#renew
-      #
-      # @example Renew whois privacy for "example.com":
-      #   client.registrar.renew_whois_privacy(1010, "example.com")
-      #
-      # @param  account_id [Integer] the account ID
-      # @param  domain_name [#to_s] The domain name
-      # @param  options [Hash]
-      # @return [Struct::WhoisPrivacy]
-      #
-      # @raise  [RequestError] When the request fails.
-      def renew_whois_privacy(account_id, domain_name, options = {})
-        endpoint = "#{whois_privacy_endpoint(account_id, domain_name)}/renewals"
-        response = client.post(endpoint, nil, options)
-
-        Dnsimple::Response.new(response, Struct::WhoisPrivacyRenewal.new(response["data"]))
       end
 
 
